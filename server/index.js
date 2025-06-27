f399a8282f62a6dd254e1df5a64c4fbf79f57f8f
abhi17bgp <iabhishekbgp21@gmail.com>
2025-06-27 11:57:54 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use("/api/", limiter);
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["*"],
     credentials: true,
   })
 );
