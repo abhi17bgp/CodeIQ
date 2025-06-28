8df07df3ad6bfed1a7f2bbb7b943678c21223a26
Abhishek Anand <156055557+abhi17bgp@users.noreply.github.com>
2025-06-28 10:59:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // Add this line to enable class-based dark mode
+  darkMode: "class", // Enable class-based dark mode
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        swingIn: "swingIn 1.5s ease-out forwards",
+        ropeDrop: "ropeDrop 1s ease-out forwards",
+        ropeBounce: "ropeBounce 1.5s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
